238c2d446734b5b0d509ebec05af5a02bc106941
ShaluSeeni <shalu.seeni7@gmail.com>
2024-04-13 18:20:11 +0800

    There was an error, menu was displaying twice that has been solved

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const contentContainer = document.querySelector('.content');
+document.querySelectorAll('.menu-link').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        // Get the href attribute of the clicked anchor link
+        const pageUrl = this.getAttribute('href');
 
-    document.querySelectorAll('.menu-link').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
+        // Check if the clicked link is not for index.html (Home)
+        if (!pageUrl.endsWith("index.html")) {
+            // Prevent the default behavior for non-Home links
             e.preventDefault();
-
-            // Get the href attribute of the clicked anchor link
-            const pageUrl = this.getAttribute('href');
 
             // Fetch the content of the target HTML file
             fetch(pageUrl)
                 .then(response => response.text())
                 .then(html => {
-                    // Replace the content of the content container with the fetched content
-                    contentContainer.innerHTML = html;
+                    // Replace the content of the content section with the fetched content
+                    document.querySelector('.content').innerHTML = html;
                 })
                 .catch(error => console.error('Error fetching page:', error));
-        });
+        }
     });
 });
