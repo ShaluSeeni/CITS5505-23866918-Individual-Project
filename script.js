192305739e6b5bf472e4a644fb29a1b27346a330
ShaluSeeni <shalu.seeni7@gmail.com>
2024-04-14 10:35:03 +0800

    Added AJAX Request in homepage

--- script.js
+++ script.js
@@ -19,3 +19,33 @@ document.querySelectorAll('.menu-link').forEach(anchor => {
         }
     });
 });
+
+// Function to make AJAX request to NekosBest API and display the image
+function fetchAndDisplayNekoImage() {
+    const apiUrl = 'https://nekos.best/api/v2/neko';
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Access the image URL from the first element in the results array
+            const imageUrl = data.results[0].url;
+            const imageElement = document.getElementById('nekoImage');
+            imageElement.className = 'neko-image'; 
+            if (imageElement && imageUrl) { // Ensure element and URL exist
+                imageElement.src = imageUrl;
+            } else {
+                console.error('Missing image element or URL');
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
+
+
+// Call the fetchAndDisplayNekoImage function to fetch and display the image
+fetchAndDisplayNekoImage();
